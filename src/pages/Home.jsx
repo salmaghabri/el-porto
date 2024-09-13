434ab3be1372a5d826b2c4915fe477ba75eafc39
salmaghabri <salma.ghabri@insat.ucar.tn>
2024-09-13 19:25:50 +0100

    ah bon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,13 @@ export default function Home() {
     <div className="relative  h-screen ">
       <Sea />
       <div
-        className={` flex flex-col absolute top-[33.9vh] left-1/3  p-8 z-20`}
+        className={` flex flex-col absolute top-[34.6vh] left-1/3  p-8 z-20`}
       >
         <div className="name font-semibold text-4xl sm:text-6xl md:text-7xl lg:text-8xl  tracking-tighter font-Cinzel text-transparent bg-clip-text bg-gradient-to-r from-lime via-cream to-lime ">
-          Salma{" "}
-          <span className="font-Vioda text-lime opacity-0 md:opacity-100">
-            {" "}
-            *{" "}
-          </span>
-          Ghabri
+          Salma <span className="ml-16">Ghabri</span>
         </div>
         <p
-          className={`sm:text-lg md:text-xl lg:text-2xl  text-transparent font-medium bg-clip-text bg-gradient-to-r from-lime via-lavender to-lime self-end`}
+          className={`sm:text-lg md:text-xl lg:text-2xl  text-transparent  bg-clip-text bg-gradient-to-tl from-cherry via-obsidian to-lime self-end font-Cinzel font-bold`}
         >
           An engineer (in the making).
         </p>
